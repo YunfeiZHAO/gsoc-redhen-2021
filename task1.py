ee4581f693360f55d6a313dead5b1098ccfe49b6
YunfeiZHAO <moizhaoyunfei@gmail.com>
2021-03-03 00:18:39 +0100

    task1 finished

--- task1.py
+++ task1.py
@@ -15,32 +15,60 @@ like at moment T+1ms, T+33.36 ms, T+1 s . Test and provide input-output example.
 """
 
 
-# 1 Extract images from a video at different time stamps.
-def extract_image(path, time):
+# Preprocessing for step 1 and 2
+def extract_image(path, time, x, y):
     """
     Extract image from a video
     :param path: The file path to the video
     :param time: the given time of the image to be captured in ms
-    :return:
+    :return: The cropped image in grayscale
     """
     vidcap = cv2.VideoCapture(path)
     vidcap.set(cv2.CAP_PROP_POS_MSEC, time)
     success, image = vidcap.read()
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     if success:
-        # cv2.imshow(f"{time}sec", gray_image)
-        # cv2.waitKey(0)
-        # croped_image = gray_image[105:105+512, 384:384+512]
-        cv2.imshow("croped gray image", gray_image)
+        cropped_image = gray_image[x:x+512, y:y+512]
+        cv2.imshow("croped gray image", cropped_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        return gray_image
+        return cropped_image
     else:
         print("unsuccessful image extraction")
 
 
+def extract_four_images(path, time=253500, x=0, y=250):
+    """
+    Extract fours images of T-33.36 ms, T+1ms, T+33.36 ms, T+1s, from a video
+    :param path: The file path to the video
+    :param time: the given time of the image to be captured in ms
+    :return: The cropped image in grayscale
+    """
+    vidcap = cv2.VideoCapture(path)
+    for t in [-33.36, 0, 1, 33.36, 1000]:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, time + t)
+        success, image = vidcap.read()
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        if success:
+            cropped_image = gray_image[x:x+512, y:y+512]
+            cv2.imwrite(f'./{t}.png', cropped_image)
+        else:
+            print("unsuccessful image extraction")
+
+
+# optical flow calculated from the images at moments T - 33.36 milliseconds and T
+'''
+Hypothesis
+We have two images takenith a little time difference
+1 The intensity of points are constant on both images 
+2 There is only a little displacement of object between images
+'''
+def gradient
+
+
 def main():
-    # extract_image('./long shoot.mkv', 26000)
+    # extract_four_images('3 Point contest 2015.mp4')
+
     pass
 
 
